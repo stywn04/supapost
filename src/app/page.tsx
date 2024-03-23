a8678a12ca8b1e625bf0a9188aaeea93ae0336bf
stywn04 <stywn.jar@gmail.com>
2024-03-24 00:27:07 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+  return (
+    <main className="min-h-screen bg-zinc-950 text-white p-20">
+      <h1 className="font-bold text-2xl">Hello world!</h1>
+    </main>
+  );
+}
