b4e2b4375b10a54414ff88b8c2fcd990095c8293
stywn04 <stywn.jar@gmail.com>
2024-03-24 17:00:34 +0700

    feat: searhc post page

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,13 +1,56 @@
+import { Search } from "lucide-react";
+import { redirect } from "next/navigation";
+import { SearchPosts } from "@/components/search-posts";
+import { Suspense } from "react";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Search",
 };
 
-export default function SearchPage() {
+interface SearchPageProps {
+  searchParams: { query: string | undefined };
+}
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const query = searchParams.query ?? "";
+  async function searchQuery(formData: FormData) {
+    "use server";
+    const query = String(formData.get("query"));
+    if (query.length < 1) {
+      return;
+    }
+    redirect(`/search?query=${query}`);
+  }
   return (
     <main>
-      <h1>Search</h1>
+      <form
+        action={searchQuery}
+        className="border border-slate-700 rounded-md "
+      >
+        <fieldset className="px-5 flex items-center justify-between">
+          <input
+            name="query"
+            placeholder="search"
+            type="text"
+            className="w-full py-2 bg-transparent outline-none"
+          />
+          <button>
+            <Search />
+          </button>
+        </fieldset>
+      </form>
+      {query.length > 0 && (
+        <Suspense
+          fallback={
+            <div className="flex items-center justify-center py-10 animate-pulse">
+              <p>searching for {query}....</p>
+            </div>
+          }
+          key={query}
+        >
+          <SearchPosts query={query} />
+        </Suspense>
+      )}
     </main>
   );
 }
