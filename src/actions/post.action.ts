272729e6576f8603de5f5caeacb9a3d3a84a26cc
stywn04 <stywn.jar@gmail.com>
2024-03-24 13:40:03 +0700

    feat: get all posts, like, and pagination

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -3,6 +3,7 @@
 import { createClient } from "@/libs/supabase/server";
 import { getCurrentUser } from "./user.action";
 import { revalidatePath } from "next/cache";
+import { notFound } from "next/navigation";
 
 export async function submitPostAction(content: string, image: string | null) {
   const { id } = await getCurrentUser();
@@ -18,4 +19,86 @@ export async function submitPostAction(content: string, image: string | null) {
 
   revalidatePath("/posts");
   return { isError: false, message: "Post submitted!" };
+}
+
+export async function getAllPostsAction(page: number) {
+  const supabase = createClient();
+  const { count, error: countError } = await supabase
+    .from("post")
+    .select("*", { count: "exact", head: true });
+
+  if (countError) {
+    throw Error(countError.message);
+  }
+  const totalPages = Math.ceil((count ?? 0) / 8);
+  const from = (page - 1) * 8;
+  const to = from + 7;
+
+  const { data, error } = await supabase
+    .from("post")
+    .select(
+      `
+    *,
+    user(name,username,avatar),
+    like(id,user_id),
+    comment(content,user(name,username,avatar))
+  `
+    )
+    .range(from, to)
+    .order("created_at", { ascending: false });
+
+  if (!data) {
+    throw error.message;
+  }
+  return { totalPages, data };
+}
+export async function getPostByIdAction(id: string) {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("post")
+    .select(
+      `
+      *,
+      user(name,username,avatar),
+      like(id,user_id),
+      comment(content,user(name,username,avatar))
+    `
+    )
+    .eq("id", id)
+    .limit(1)
+    .single();
+
+  if (error) {
+    throw Error(error.message);
+  }
+
+  if (!data) {
+    return notFound();
+  }
+
+  return data;
+}
+
+export type PostType = Awaited<ReturnType<typeof getPostByIdAction>>;
+
+interface LikePostActionArgs {
+  post_id: string;
+  like: { id: string; user_id: string }[];
+  pathname: string;
+}
+export async function likePostAction({
+  post_id,
+  like,
+  pathname,
+}: LikePostActionArgs) {
+  const supabase = createClient();
+  const { id } = await getCurrentUser();
+
+  const userExist = like.find((l) => l.user_id === id);
+
+  userExist
+    ? await supabase.from("like").delete().eq("id", userExist.id)
+    : await supabase.from("like").insert({ user_id: id, post_id });
+
+  revalidatePath(pathname);
 }
