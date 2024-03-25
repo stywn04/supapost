9d853ef8768c8e3b621760ec10b962b83a94b268
stywn04 <stywn.jar@gmail.com>
2024-03-25 07:54:36 +0700

    feat: user link to profile

--- src/components/comment/user.tsx
+++ src/components/comment/user.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface CommentUserProps {
   avatar: string;
   name: string;
@@ -5,15 +7,17 @@ interface CommentUserProps {
 }
 export function CommentUser({ avatar, name, username }: CommentUserProps) {
   return (
-    <section className="flex items-center gap-2">
-      <div className="w-8 h-8 rounded-full overflow-hidden">
-        <img src={avatar} alt={username} />
-      </div>
+    <Link href={`/profile/${username}`} className="inline">
       <div className="flex items-center gap-2">
-        <h4 className="font-semibold text-zinc-200">{name}</h4>
-        &bull;
-        <p className="font-light text-zinc-700">@{username}</p>
+        <div className="w-8 h-8 rounded-full overflow-hidden">
+          <img src={avatar} alt={username} />
+        </div>
+        <div className="flex items-center gap-2">
+          <h4 className="font-semibold text-zinc-200">{name}</h4>
+          &bull;
+          <p className="font-light text-zinc-700">@{username}</p>
+        </div>
       </div>
-    </section>
+    </Link>
   );
 }
